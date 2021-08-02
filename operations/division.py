8a95d59aafcaf15d0be9de4b9b760c947c0af6bf
nightmare <antonmotchanyi@gmail.com>
2021-08-02 14:09:39 +0300

    Implement division operation

---
+++ operations/division.py
@@ -0,0 +1,10 @@
+# Division by two numbers
+def division(number_1, number_2):
+    if float(number_1) or float(number_2):
+        result = number_1 / number_2
+    else:
+        result = number_1 // number_2
+    return result
+
+
+print(division(8.2, 4))
